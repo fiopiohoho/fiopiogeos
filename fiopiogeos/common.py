21f125ffc89ace21d929d9e7b10fb3fd4dc2a2ca
Ferdinan Aurifio <ferdinan.aurifio@gmail.com>
2024-12-10 15:27:24 +0700

    input func random number

--- fiopiogeos/common.py
+++ fiopiogeos/common.py
@@ -5,3 +5,19 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+
+import random
+
+def random_number():
+    """
+    Generates a random number between 1 and 100.
+    Returns:
+        int: A random integer.
+    """
+    return random.randint(1, 1000)
+
+# Example usage
+print(random_number())
+
+
